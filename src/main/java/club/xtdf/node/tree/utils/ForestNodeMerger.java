b9326996619a1913819f3d4682b8b03e8a9350f0
yangyi <yangyi@allcorehatress.com>
2022-06-15 17:01:35 +0800

    更新文档

--- src/main/java/club/xtdf/node/tree/utils/ForestNodeMerger.java
+++ src/main/java/club/xtdf/node/tree/utils/ForestNodeMerger.java
@@ -17,7 +17,10 @@ public class ForestNodeMerger<T> implements Serializable {
     /**
      * 将节点数组归并为一个森林（多棵树）（填充节点的children域） 时间复杂度为O(n^2)
      *
-     * @param items 节点域
+     * @param items    节点域
+     * @param key      节点主键
+     * @param parent   节点父级
+     * @param children 子节点集合
      * @return 多棵树的根节点集合
      */
     public List<T> merge(List<T> items, NodeFunction<T, ?> key, NodeFunction<T, ?> parent, NodeFunction<T, ?> children) {
